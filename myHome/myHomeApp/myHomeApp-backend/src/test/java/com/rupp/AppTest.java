56c90113e62f037f394614c07a1d93ad85ef7f39
mani deepu <nukala435@gmail.com>
2020-02-06 23:38:45 +0530

    First Commit

---
+++ myHome/myHomeApp/myHomeApp-backend/src/test/java/com/rupp/AppTest.java
@@ -0,0 +1,12 @@
+package com.rupp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
