e7a7f4d3647b0fb5ed236c36217af00c04652b58
mani deepu <nukala435@gmail.com>
2020-02-09 12:03:59 +0530

    NewYearGift

--- maven-oops/src/main/java/com/epam/maven_oops/App.java
+++
@@ -1,8 +0,0 @@
-package com.epam.maven_oops;
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
--- maven-oops/src/main/java/com/epam/maven_oops/NewYearGift.java
+++
@@ -1,7 +0,0 @@
-package com.epam.maven_oops;
-
-public interface NewYearGift {
-	int getPrice();
-	int getWeight();
-	
-}
